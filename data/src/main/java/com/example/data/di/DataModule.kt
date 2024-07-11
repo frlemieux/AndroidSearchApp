cf53a5ae5656518346adbf81e619402fb4694ae0
tagask <frederic.lemieux14@gmail.com>
2024-07-11 11:33:49 +0200

    Rewrite data module injection

--- data/src/main/java/com/example/data/di/DataModule.kt
+++ data/src/main/java/com/example/data/di/DataModule.kt
@@ -1,63 +1,44 @@
 package com.example.data.di
 
-import android.content.Context
 import androidx.room.Room
 import com.example.data.local.RepoDatabase
 import com.example.data.remote.GithubApi
 import com.example.data.repository.RepoRepositoryImpl
 import com.example.domain.repository.RepoRepository
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
+import io.reactivex.rxjava3.schedulers.Schedulers.single
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-object DataModule {
-    @Provides
-    @Singleton
-    fun provideGithubRetrofit(): Retrofit {
-        val logger = HttpLoggingInterceptor()
-        logger.level = HttpLoggingInterceptor.Level.BASIC
-        val client =
-            OkHttpClient
+val dataModule =
+    module {
+        single {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+            val client = OkHttpClient.Builder().addInterceptor(logger).build()
+            Retrofit
                 .Builder()
-                .addInterceptor(logger)
+                .baseUrl("https://api.github.com/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
-        return Retrofit
-            .Builder()
-            .baseUrl("https://api.github.com/")
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        }
+
+        single {
+            val retrofit: Retrofit = get()
+            retrofit.create(GithubApi::class.java)
+        }
+
+        single {
+            Room.databaseBuilder(androidContext(), RepoDatabase::class.java, "github.db").build()
+        }
+
+        single {
+            val contactApi: GithubApi = get()
+            val contactDatabase: RepoDatabase = get()
+            RepoRepositoryImpl(contactApi, contactDatabase) as RepoRepository
+        }
     }
-
-    @Provides
-    @Singleton
-    fun provideContactApi(retrofit: Retrofit): GithubApi = retrofit.create(GithubApi::class.java)
-
-    @Provides
-    @Singleton
-    fun provideContactDatabase(
-        @ApplicationContext context: Context,
-    ): RepoDatabase =
-        Room
-            .databaseBuilder(
-                context,
-                RepoDatabase::class.java,
-                "github.db",
-            ).build()
-
-    @Provides
-    @Singleton
-    fun provideRepoRepositoryImpl(
-        contactApi: GithubApi,
-        contactDatabase: RepoDatabase,
-    ): RepoRepository = RepoRepositoryImpl(contactApi, contactDatabase)
-}
